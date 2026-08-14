aae2dc2eca13bbc8f6b891b3d16b01230c04a6a8
rambaburonte <ronterambabu@gmail.com>
2026-08-14 13:53:07 +0530

    tt

--- shagriha_backend_services/src/main/java/com/orbitekk/shagriha/user/UserRepository.java
+++ shagriha_backend_services/src/main/java/com/orbitekk/shagriha/user/UserRepository.java
@@ -4,8 +4,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-public interface UserRepository extends JpaRepository<AppUser, UUID> , CrudRepository<AppUser, UUID> {
+
+public interface UserRepository extends JpaRepository<AppUser, UUID> {
     Optional<AppUser> findByUsernameIgnoreCaseOrEmailIgnoreCase(String username, String email);
     boolean existsByUsernameIgnoreCase(String username);
     boolean existsByEmailIgnoreCase(String email);
